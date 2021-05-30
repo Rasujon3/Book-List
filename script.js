08ff536009875e846100e9910e203937f9e5cd68
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-31 02:06:07 +0600

    03. Add Books - JavaScript by Simanta Paul
    03. Add Books - JavaScript by Simanta Paul

---
+++ script.js
@@ -0,0 +1,60 @@
+// Get the UI Element
+let form = document.querySelector('#book-form');
+
+
+// Book Class
+class Book{
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class
+class UI {
+    constructor(){
+
+    }
+    addToBooklist(book){
+        let list = document.querySelector('#book-list');
+        let row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href='#' class="delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+
+    }
+    clearFields(){
+       document.querySelector("#title").value = '';
+     document.querySelector("#author").value = '';
+     document.querySelector("#isbn").value = '';
+    }
+}
+
+// Add Event Listener
+form.addEventListener('submit', newBook);
+
+
+// Define functions
+function newBook(e){
+    let title = document.querySelector("#title").value,
+     author = document.querySelector("#author").value,
+     isbn = document.querySelector("#isbn").value;
+
+     let book = new Book(title,author,isbn);
+
+     let ui = new UI();
+
+     ui.addToBooklist(book);
+
+     ui.clearFields();
+     
+
+
+    e.preventDefault();
+}
